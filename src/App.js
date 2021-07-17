7e6eadea9daca57e9b47f77d19015cfdf41c22c8
Thien <hoangthiennt2003@gmail.com>
2021-07-17 20:30:42 +0700

    update new version

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react'
+function App() {
+  return (
+    <div className="App">
+      <h1>FUN CHAP APP</h1>
+      <div className="Button_main">
+        <button>
+          <p>Dang nhap bang Google</p>
+        </button>
+        <button onClick={}>
+          <p>Dang nhap bang Facebook</p>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
